d7b628b4f6e9c3e82f40ed4e089cf2fb3d722e3b
sarahpicard <sarahannrey225@gmail.com>
2022-04-10 16:39:14 -0400

    adjust navigation bar

--- src/pages/MyPets/MyPets.jsx
+++ src/pages/MyPets/MyPets.jsx
@@ -2,16 +2,18 @@ import { useState } from 'react'
 
 import PetCard from '../../components/PetCard/PetCard'
 
+import styles from './MyPets.module.css'
+
 const MyPets = (props) => {
   return (
-    <>
+    <div className={styles.container}>
       <h1>These are all of my pets</h1>
       <section>
         {props.pets.map((pet) => (
           <PetCard pet={pet} key={pet.id} />
         ))}
       </section>
-    </>
+    </div>
   )
 }
 
