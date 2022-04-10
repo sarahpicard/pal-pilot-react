f3f970408ea0bf3609ef9a029334edc65d031514
sarahpicard <sarahannrey225@gmail.com>
2022-04-10 12:47:35 -0400

    readjust navbar basic link styling and home link

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -1,4 +1,7 @@
 import { Link } from 'react-router-dom'
+import { FaPaw } from 'react-icons/fa'
+import styles from './NavBar.module.css'
+
 
 const NavBar = (props) => {
   return (
@@ -6,20 +9,21 @@ const NavBar = (props) => {
       {props.user ?
         <nav>
           <ul>
-            <li>Welcome, {props.user.name}</li>
-            <li><Link to="" onClick={props.handleLogout}>LOG OUT</Link></li>
-            <li><Link to="/changePassword">Change Password</Link></li>
-            <li><Link to="/mypets">My Pets</Link></li>
-            <li><Link to="/addpet">Add a New Pet</Link></li>
-            <li><Link to="/appointments">Appointments</Link></li>
-            <li><Link to="/myprofile">My Profile</Link></li>
+            <li><Link to="/" style={{ textDecoration: 'none' }}><FaPaw />Pal Pilot</Link></li>
+            <li><Link to="" onClick={props.handleLogout} style={{ textDecoration: 'none' }}>Log Out</Link></li>
+            <li><Link to="/changePassword" style={{ textDecoration: 'none' }}>Change Password</Link></li>
+            <li><Link to="/mypets" style={{ textDecoration: 'none' }}>My Pets</Link></li>
+            <li><Link to="/addpet" style={{ textDecoration: 'none' }}>Add a New Pet</Link></li>
+            <li><Link to="/appointments" style={{ textDecoration: 'none' }}>Appointments</Link></li>
+            <li><Link to="/myprofile" style={{ textDecoration: 'none' }}>My Profile</Link></li>
           </ul>
         </nav>
       :
         <nav>
           <ul>
-            <li><Link to="/login">Log In</Link></li>
-            <li><Link to="/signup">Sign Up</Link></li>
+            <li><Link to="/" style={{ textDecoration: 'none' }}><FaPaw />Pal Pilot</Link></li>
+            <li><Link to="/login" style={{ textDecoration: 'none' }}>Log In</Link></li>
+            <li><Link to="/signup" style={{ textDecoration: 'none' }}>Sign Up</Link></li>
           </ul>
         </nav>
       }
