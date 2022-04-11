2af9d6ff665a5506186e541255fa6bea04c57263
sarahpicard <sarahannrey225@gmail.com>
2022-04-11 16:43:26 -0400

    adjust margin on navigation bar

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -10,7 +10,7 @@ const NavBar = (props) => {
       {props.user ?
         <div>
           <ul>
-            <li className={`${styles.li} ${styles.name}`}><Link to="/" className={styles.a}><FaPaw />Pal Pilot</Link></li>
+            <li className={styles.li}><Link to="/" className={styles.a}><FaPaw />Pal Pilot</Link></li>
             <li className={styles.li}><Link to="/pets" className={styles.a}>My Pets</Link></li>
             <li className={styles.li}><Link to="/addpet" className={styles.a}>Add a New Pet</Link></li>
             <li className={styles.li}><Link to="/appointments" className={styles.a}>Appointments</Link></li>
