e5dab0e70b1ca439c416bc11ff06e20138ddd58b
sarahpicard <sarahannrey225@gmail.com>
2022-04-11 09:50:14 -0400

    stub basic appointment service functions

---
+++ src/services/appointmentService.js
@@ -0,0 +1,80 @@
+import * as tokenService from './tokenService'
+const BASE_URL = `${process.env.REACT_APP_BACKEND_SERVER_URL}/api/appointments/`
+
+
+// create an appointment 
+export const create = async (appointment) => {
+  try {
+    const res = await fetch(BASE_URL, {
+      method: "POST", 
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+      body: JSON.stringify(appointment),
+    })
+    return await res.json()
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+// see all appointments 
+export const getAll = async () => {
+  try {
+    const res = await fetch(`${BASE_URL}`)
+    return await res.json()
+  } catch (error) {
+    console.log(error)
+    throw(error)
+  }
+}
+
+
+// get one appointment 
+export const getOne = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}${id}`)
+    return await res.json()
+  } catch (error) {
+    console.log(error)
+    throw (error)
+  }
+}
+
+
+// update an appointment 
+export const update = async (appointment) => {
+  try {
+    const res = await fetch(`${BASE_URL}${appointment.id}`, {
+      method: "PUT",
+      headers: {
+        'content-type': 'application/json',
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      },
+      body: JSON.stringify(appointment)
+    })
+    return await res.json()
+  } catch (error) {
+    console.log(error)
+    throw error
+  }
+}
+
+
+// delete an appointment 
+export const deleteOne = async (id) => {
+  try {
+    const res = await fetch(`${BASE_URL}${id}`, {
+      method: "DELETE",
+      headers: {
+        'Authorization': `Bearer ${tokenService.getToken()}`
+      }
+    })
+    return await res.json()
+  } catch (error) {
+    console.log(error)
+    throw(error)
+  }
+}
