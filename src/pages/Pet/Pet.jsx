d1eac4da478611c9c6cbeccfce5388cb6281603d
sarahpicard <sarahannrey225@gmail.com>
2022-04-12 17:52:36 -0400

    add basic div styling to pet detail page

--- src/pages/Pet/Pet.jsx
+++ src/pages/Pet/Pet.jsx
@@ -3,6 +3,8 @@ import { useParams } from 'react-router-dom'
 
 import { getOne } from '../../services/petService'
 
+import styles from './Pet.module.css'
+
 const Pet = (props) => {
   const { id } = useParams()
   const [pet, setPet] = useState(null)
@@ -19,13 +21,18 @@ const Pet = (props) => {
 
   return (
     <>
-      <h1>{pet.name}</h1>
-      <h5>Breed: {pet.breed}</h5>
-      <p>Age: {pet.age}</p>
-      <p>Weight: {pet.weight}</p>
-      <p>{pet.medications.length ? pet.medications : 'no medications'}</p>
-      <p>{pet.vaccines.length ? pet.vaccines : 'no vaccines'}</p>
-      <p>{pet.allergies.length ? pet.allergies : 'no allergies'}</p>
+      <div className={styles.div}>
+        <div className={styles.whole}>
+          <h1>{pet.name}</h1>
+          <img src="https://i.imgur.com/sflSwK0.png" alt="puppy_picture" style={{ "width": "300px", "height": "300px"}}/>
+          <h5>Breed: {pet.breed}</h5>
+          <p>Age: {pet.age}</p>
+          <p>Weight: {pet.weight}</p>
+          <p>{pet.medications.length ? pet.medications : 'no medications'}</p>
+          <p>{pet.vaccines.length ? pet.vaccines : 'no vaccines'}</p>
+          <p>{pet.allergies.length ? pet.allergies : 'no allergies'}</p>
+        </div>
+      </div>
     </>
   )
 }
