7c2830aa0e25295395737c3a603db23f5d3715db
sarahpicard <sarahannrey225@gmail.com>
2022-04-12 12:53:14 -0400

    adjust appointment page styling for desktop

--- src/components/AppointmentCard/AppointmentCard.jsx
+++ src/components/AppointmentCard/AppointmentCard.jsx
@@ -1,9 +1,13 @@
+import styles from './AppointmentCard.module.css'
+
 const AppointmentCard = (props) => {
   return (
     <>
-      <h3>Appointment Name: {props.appointment.name}</h3>
-      <p>Description: {props.appointment.description}</p>
-      <p>Address: {props.appointment.address}</p>
+      <div className={styles.whole}>
+        <h3>Appointment Name: {props.appointment.name}</h3>
+        <p>Description: {props.appointment.description}</p>
+        <p>Address: {props.appointment.address}</p>
+      </div>
     </>
   )
 }
