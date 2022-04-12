dcf4e7f65b9dff686aee6f208818f99e2840f081
sarahpicard <sarahannrey225@gmail.com>
2022-04-12 14:01:03 -0400

    add handleToggle to pet card

--- src/components/PetCard/PetCard.jsx
+++ src/components/PetCard/PetCard.jsx
@@ -1,13 +1,34 @@
 import { Link } from 'react-router-dom'
+import { useState } from 'react'
+
+import styles from './PetCard.module.css'
 
 const PetCard = (props) => {
+  const [isActive, setActive] = useState("false")
+
+  const handleToggle = () => {
+    setActive(!isActive);
+  }
+
   return (
     <>
-      <h2>{props.pet.name}</h2>
-      <p>{props.pet.animal_type}</p>
-      <p>{props.pet.breed}</p>
-      <p>Age: {props.pet.age}</p>
-      <p>Weight: {props.pet.weight}</p>
+      <div 
+        className={isActive ? styles.whole : styles.flipped}
+        onClick={handleToggle}
+      >
+        <div className={styles.card}>
+          <div className={styles.front}>
+            <h2>{props.pet.name}</h2>
+            <img src="https://i.imgur.com/11OvKIB.jpg" alt="puppy" style={{ "width": "100px", "height": "100px"}}/>
+          </div>
+          <div className={styles.back}>
+            <p>{props.pet.animal_type}</p>
+            <p>{props.pet.breed}</p>
+            <p>Age: {props.pet.age}</p>
+            <p>Weight: {props.pet.weight}</p>
+          </div>
+        </div>
+      </div>
     </>
   )
 }
