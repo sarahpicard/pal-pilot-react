b628f7eac951c080f6db874d04cfc693f9ce0ba8
sarahpicard <sarahannrey225@gmail.com>
2022-04-14 16:36:17 -0400

    adjust media query for nav and pet list page

--- src/components/ProtectedRoute/ProtectedRoute.jsx
+++
@@ -1,9 +0,0 @@
-import { Navigate } from 'react-router-dom'
-const ProtectedRoute = ({ user, children }) => {
-  if (user.id === user.pets.profile_id) { 
-    return <Navigate to="/" /> 
-  }
-  return children
-}
-
-export default ProtectedRoute
--- src/pages/MyPets/MyPets.jsx
+++ src/pages/MyPets/MyPets.jsx
@@ -2,16 +2,10 @@ import PetCard from '../../components/PetCard/PetCard'
 
 import styles from './MyPets.module.css'
 
-const MyPets = (props, i) => {
-  // console.log("id",props.user.id)
-  // console.log("type user id", typeof(props.user.id))
-  // console.log("pet prof id", props.pets.profile_id)
-  // console.log("type of pet id", typeof(props.pets[0].profile_id))
-  const userId = props.user.id
+const MyPets = (props) => {
 
   return (
     <div>
-      {/* {props.user.id === props.pets?.profile_id ?  */}
           <section className={styles.section}>
             {props.pets.map((pet) => {
               return pet.profile_id === props.user.id ?
@@ -20,19 +14,6 @@ const MyPets = (props, i) => {
               <p className={styles.empty}>You don't have any pets yet!</p>
             })}
           </section>
-         {/* <section className={styles.section}>
-           {props.pets.map((pet) => (
-              <PetCard pet={pet} key={pet.id} images={props.images} />
-          ))}
-        </section> */}
-         {/* :
-         <h1>You don't have any pets yet! 🙀</h1>
-       } */}
-      {/* <section>
-        {props.pets.map((pet) => {
-          return pet.profile_id
-        })}
-      </section> */}
     </div>
   )
 }
